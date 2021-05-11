c94d429370b8402208f9afa7374d49bc4009cc16
Matteo <matteo.mesagonzalez@epfl.ch>
2021-05-11 10:01:07 +0200

    creating movement files

---
+++ movement.c
@@ -0,0 +1,40 @@
+#include <main.h>
+#include <motors.h>
+#include <audio_processing.h>
+#include <arm_math.h>
+
+
+static float dist; 
+
+#define MAX_SUM_ERROR 100
+
+
+int16_t pi_regulator(float distance, float goal){
+
+	float error = 0;
+	float speed = 0;
+
+	static float sum_error = 0;
+
+	error = distance - goal;
+
+	//disables the PI regulator if the error is to small
+	//this avoids to always move as we cannot exactly be where we want and 
+	//the camera is a bit noisy
+	if(fabs(error) < ERROR_THRESHOLD){
+		return 0;
+	}
+
+	sum_error += error;
+
+	//we set a maximum and a minimum for the sum to avoid an uncontrolled growth
+	if(sum_error > MAX_SUM_ERROR){
+		sum_error = MAX_SUM_ERROR;
+	}else if(sum_error < -MAX_SUM_ERROR){
+		sum_error = -MAX_SUM_ERROR;
+	}
+
+	speed = KP * error + KI * sum_error;
+
+    return (int16_t)speed;
+}
